b58c8bb85c1e746cf75d32d6edaed1a6543f80ef
Justin <justin.vuong6@gmail.com>
2025-03-28 15:21:41 -0600

    fixed required fields for password and username

--- frontend/lab9/src/login.js
+++ frontend/lab9/src/login.js
@@ -1,79 +1,80 @@
-import React, {useState, useEffect} from "react"
-import "./login.css"
+import React, { useState, useEffect } from "react";
+import "./login.css";
 
 function Login() {
-    const [username, setUsername] = useState("")
-    const [password, setPassword] = useState("")
-    const [message, setMessage] = useState("")
-    const [loginSuccessful, setLoginSuccessful] = useState(false)
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const [loginSuccessful, setLoginSuccessful] = useState(false);
 
-    
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-    const handleSubmit = async (event) => {
-        event.preventDefault()
-        try {
-            const response = await fetch('http://127.0.0.1:5000/validate_login', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json'},
-                body: JSON.stringify({username: username, password: password}),
-            });
-            const data = await response.json();
-            setLoginSuccessful(data.success)
-            if (data.success === false) {
-                setMessage(data.message)
-                console.log("false")
-            }
-        }
-        catch (error) {
-            console.error('Error fetching prediction:', error);
-        }
-    };
+    if (!username || !password) {
+      setMessage("Username and password are required.");
+      return;
+    }
 
-    useEffect(() => {
-        if (loginSuccessful) {
-            window.location.href = "/predict";
-        }
-    }, [loginSuccessful]);
+    try {
+      const response = await fetch("http://127.0.0.1:5000/validate_login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username: username, password: password }),
+      });
+      const data = await response.json();
+      setLoginSuccessful(data.success);
+      if (data.success === false) {
+        setMessage(data.message);
+        console.log("false");
+      }
+    } catch (error) {
+      console.error("Error fetching prediction:", error);
+    }
+  };
 
-    return (
-        <div className = "form-container">
-            <h2>Login</h2>
-            <div className="form-item">
-                <label htmlFor="username">Username</label>
-                <form onSubmit = {handleSubmit}>
-                    <input
-                        type = "text"
-                        placeholder="Username"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                        />
-                </form>
-            </div>
-            <div className="form-item">
-                <label htmlFor="Password">Password</label>
-                <form onSubmit={handleSubmit}>
-                    <input
-                    type="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                    />
-                </form>
-            </div>
-            <div className="form-item">
-                <form onSubmit={handleSubmit}>
-                    <button type="submit">Login</button>
-                </form>
-            </div>
-            {!loginSuccessful && message && (
-                <div className='error-message'>
-                    Error: {message}
-                </div>
-            )}
-        </div>
+  useEffect(() => {
+    if (loginSuccessful) {
+      window.location.href = "/predict";
+    }
+  }, [loginSuccessful]);
 
-    )
+  return (
+    <div className="form-container">
+      <h2>Login</h2>
+      <div className="form-item">
+        <label htmlFor="username">Username</label>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </form>
+      </div>
+      <div className="form-item">
+        <label htmlFor="Password">Password</label>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </form>
+      </div>
+      <div className="form-item">
+        <form onSubmit={handleSubmit}>
+          <button type="submit">Login</button>
+        </form>
+      </div>
+      {!loginSuccessful && message && (
+        <div className="error-message">Error: {message}</div>
+      )}
+    </div>
+  );
 }
 
 export default Login;
