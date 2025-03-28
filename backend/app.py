234301cb40d3d22c02632336329a65422c166b18
Charlie Lang <charlie.lang@ucalgary.ca>
2025-03-28 11:35:23 -0600

    add prediction endpoint for house price using trained model

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+import joblib
+import pandas as pd
 
 app = Flask(__name__)
 CORS(app)
@@ -29,8 +31,38 @@ def validate_login():
         return jsonify({"success": False, "message": "Invalid credentials!"}), 401
     
 @app.route('/predict_house_price', methods=['POST'])
-def get_resource():
-    return jsonify({"message": "Skeleton endpoint to be implemented"})
+def predict_house_price():
+
+    model = joblib.load("./src/random_forest_model.pkl")
+    data = request.json
+
+    cats = True if 'pets' in data and data['pets'] else False
+    dogs = True if 'pets' in data and data['pets'] else False
+
+    sample_data = [
+        data['city'],
+        data['province'],
+        float(data['latitude']),
+        float(data['longitude']),
+        data['lease_term'],
+        data['type'],
+        float(data['beds']),
+        float(data['baths']),
+        float(data['sq_feet']),
+        data['furnishing'],
+        data['smoking'],
+        cats,
+        dogs
+    ]
+
+    sample_df = pd.DataFrame([sample_data], columns=[
+        'city', 'province', 'latitude', 'longitude', 'lease_term',
+        'type', 'beds', 'baths', 'sq_feet', 'furnishing',
+        'smoking', 'cats', 'dogs'
+    ])
+
+    predicted_price = model.predict(sample_df)
+    return jsonify({"predicted_price": float(predicted_price[0])})
 
 if __name__ == '__main__':
     app.run(debug=True)
